77113c281984cdc17305142e099b1d38b0a50141
Ramiro Reinoso <ramiro.reinoso@verizon.net>
2023-05-16 14:24:25 -0400

    Add project files.

---
+++ EmpowerRR_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerRR_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerRR_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("250E610D-FB39-4E0D-A331-C3A9E9A2D085")]
+[assembly: AssemblyVersion("1.0.0.0")]
